3fcccb544d3c9c75e18a0117587b4cb3dabc8395
DaniloRad <42680355+DaniloRad@users.noreply.github.com>
2018-11-28 23:23:50 +0100

    Add files via upload
    Resolve conflict

--- ModalWindow.js
+++ ModalWindow.js
@@ -3,16 +3,19 @@
 
 function modalWindow (e) {
 
-    var divNode=e.target;
+    
+    let divNode=e.target;
         while (!divNode.classList.contains("movie")) {
             divNode=divNode.parentElement;
         }
         divNode=divNode.childNodes;
         //sad je divNode sig movie
- var modalWindow=document.getElementsByClassName("modal_window")[0];
- var modalWindowImg=document.getElementsByClassName("modal_window_img")[0];
- var modalWindowAbout=document.getElementsByClassName("modal_windows_about")[0];
-    document.getElementsByClassName("noneClick")[0].style.pointerEvents="none";
+ let modalWindow=document.getElementsByClassName("modal_window")[0];
+ let modalWindowImg=document.getElementsByClassName("modal_window_img")[0];
+ let modalWindowAbout=document.getElementsByClassName("modal_windows_about")[0];
+
+document.getElementsByClassName("noneClick")[0].style.pointerEvents="none";
+
  modalWindow.style.display="flex";
  modalWindowImg.innerHTML=divNode[0].innerHTML;
  modalWindowAbout.innerHTML=divNode[1].innerHTML;
@@ -20,9 +23,9 @@ function modalWindow (e) {
 
 
     }   
-    var best = document.getElementsByClassName("best")[0];
+    let best = document.getElementsByClassName("best")[0];
     best.addEventListener ("click",modalWindow);
-    var exit = document.getElementsByClassName("exit")[0];
+    let exit = document.getElementsByClassName("exit")[0];
     exit.addEventListener ("click",Exit);
     function Exit (e) {
 
