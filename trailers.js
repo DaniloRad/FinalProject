096f33c04a65871a9bff1fca156fcca385905538
DaniloRad <42680355+DaniloRad@users.noreply.github.com>
2018-11-29 17:59:54 +0100

    Add files via upload
    YOUTUBE TRAILER

---
+++ trailers.js
@@ -0,0 +1,37 @@
+(function () {
+    document.getElementById("search_button").addEventListener("click", test)
+
+    function test() {
+
+        let input = document.getElementById("search_input").value;
+
+        let link = "https://www.youtube.com/embed/";
+
+        let forFetch = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + input + "trailer official" + "&type=video&key=AIzaSyDdlhYgPFwejy_bGJcfLEL68iFdr83o1vA";
+        fetch(forFetch)
+            .then(function (response) {
+
+                return response.json();
+
+            })
+            .then(function (myJson) {
+                return myJson.items;
+            })
+            .then(function (items) {
+                let iframe;
+                let div = document.getElementsByClassName("video")[0];
+                div.innerHTML = "";
+                for (let i = 0; i < 5; i++) {
+                    console.log(items[i])
+                    iframe = document.createElement("iframe");
+                    iframe.setAttribute("src", link + items[i].id.videoId);
+                    div.append(iframe);
+
+
+                }
+
+            })
+
+    };
+    test();
+})();
