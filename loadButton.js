931686aca9dc257b2ecec33b89f4f4b70509ca9f
DaniloRad <42680355+DaniloRad@users.noreply.github.com>
2018-12-02 21:47:54 +0100

    Delete loadButton.js

--- loadButton.js
+++
@@ -1,9 +0,0 @@
-
-function load() {
-    var x = document.getElementsByClassName("load");
-    if (x.style.display === "grid") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "grid";
-    }
-}
