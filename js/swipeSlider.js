e81a4d473cc8d4f21d63611c99f24cb5a41daa87
DaniloRad <danilor888@gmail.com>
2018-12-04 16:22:59 +0100

    Deleted slider.css add border-radius, deleted console log

--- js/swipeSlider.js
+++ js/swipeSlider.js
@@ -3,14 +3,10 @@ function main() {
     body.addEventListener("touchstart", swipeDown);
     body.addEventListener("touchend", swipeUp);
     var swipeX;
-    var swipeY;
-    var swipeRight = 0;
-
+    
     function swipeDown(e) {
-        swipeRight = 0;
         swipeX = e.touches[0].clientX;
-        swipeY = e.touches[0].clientY;
-        console.log(swipeX)
+        
 
     }
 
@@ -18,9 +14,7 @@ function main() {
 
         var xx = e.changedTouches[0].clientX;
 
-        var yy = e.changedTouches[0].clientY;
-        console.log(e.clientX)
-        console.log(swipeX - xx)
+     
         if ((Math.abs(swipeX - xx) > 100)) {
 
             if (swipeX - xx < 0) {
@@ -34,7 +28,6 @@ function main() {
                 }
             }
 
-            SwipePage(currNum);
 
         }
 
