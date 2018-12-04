ec6752c00f3ce1cb6004fdc85ebd98a67fc2bcf8
DaniloRad <danilor888@gmail.com>
2018-12-04 17:16:38 +0100

    Fixed error in loadmovies.js there was >> from conflict left

--- js/loadmovies.js
+++ js/loadmovies.js
@@ -184,10 +184,7 @@ function main(genre) {
             for (let j = 0; j < arrayOfGenresAPI.length; j++) {
                 if (arrayMovie[i] === arrayOfGenresAPI[j].id) {
                     genreName[k++] = arrayOfGenresAPI[j].name;
-<<<<<<< HEAD
                     
-=======
->>>>>>> master
                 }
             }
         }
